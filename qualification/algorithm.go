5e346d28141fb1ab812b9f2ef94518aae207e92b
Timothy C. Lam <geroginalam@hotmail.com>
2020-01-07 21:25:27 +0800

    update documentation

---
+++ qualification/algorithm.go
@@ -0,0 +1,12 @@
+package main
+
+// Main algorithm
+func (p *problem) algorithm1() {
+
+}
+
+// Calculate answers score and store result in p.score
+// Access answer struct with p.answers (type is a slice of answer)
+func (p *problem) calcScore() {
+
+}
